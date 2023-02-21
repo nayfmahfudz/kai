150b67bb243a180d9196364f156a7d4af8a0175a
naeef <mochammadnayf@gmail.com>
2023-02-21 16:46:23 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/listGerbang.dart
    
    Changes not staged for commit:
    	modified:   lib/component/cardoutlet.dart
    	modified:   lib/component/fom.dart
    	modified:   linux/flutter/generated_plugin_registrant.cc
    	modified:   linux/flutter/generated_plugin_registrant.h
    	modified:   linux/flutter/generated_plugins.cmake
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugin_registrant.h
    	modified:   windows/flutter/generated_plugins.cmake

--- lib/listGerbang.dart
+++ lib/listGerbang.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:kai/component/cardoutlet.dart';
+import 'package:kai/component/fom.dart';
 import 'package:kai/setting.dart';
 
 class ListGerbang extends StatefulWidget {
@@ -12,12 +13,12 @@ class ListGerbang extends StatefulWidget {
 
 Future gerbang() async {
   try {
-    var response =
-        await Dio().get("http://api-kai-qc.arthoize.com/api/v1/carriage",
-            options: Options(headers: {
-              "Content-Type": "application/json",
-              "Authorization": "Bearer $key",
-            }));
+    var response = await Dio().get(
+        "http://api-kai-qc.arthoize.com/api/v1/carriage-checklist         ",
+        options: Options(headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $key",
+        }));
     print(response);
     return response.data["data"];
   } catch (e) {
@@ -25,20 +26,34 @@ Future gerbang() async {
   }
 }
 
+var searchcontroller = TextEditingController();
+
 class _ListGerbangState extends State<ListGerbang> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: FutureBuilder(
+        body: ListView(
+      padding: const EdgeInsets.all(20.0),
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      children: [
+        search(searchcontroller, context),
+        SizedBox(
+          height: 15,
+        ),
+        FutureBuilder(
             future: gerbang(),
             builder: ((context, snapshot) {
               if (snapshot.hasData) {
                 return ListView.builder(
+                    physics: ScrollPhysics(),
                     itemCount: snapshot.data.length ?? 0,
                     shrinkWrap: true,
                     itemBuilder: (context, i) => Cardoutlet(snapshot.data[i]));
-              }
-            })));
+              } else
+                return Container();
+            })),
+      ],
+    ));
   }
 }
